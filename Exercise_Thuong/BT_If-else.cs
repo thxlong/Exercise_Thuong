af6c8deecef2415f3edc86cfd71a55d2ac51a8e8
Long Nguyen <ntl.284@gmail.com>
2022-07-17 16:55:08 +0700

    Add project files.

---
+++ Exercise_Thuong/BT_If-else.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Thuong
+{
+    public class BT_If_else
+    {
+        public void ex_5()
+        {
+        
+            float height, weight, BMI;
+            Console.Write("Please enter height value (m): ");
+            height = float.Parse(Console.ReadLine());
+
+            Console.Write("Please enter weight value (kg): ");
+            weight = float.Parse(Console.ReadLine());
+            BMI = (float)(weight / (Math.Pow(height, 2))); // BMI = Can Nang/(ChieuCao ^ 2)
+
+            Console.WriteLine("Your BMI = " + BMI);
+
+            if (BMI < 15)
+                Console.WriteLine("Thân hình quá gầy");
+            else if (BMI >= 15 && BMI < 16)
+                Console.WriteLine("Thân hình gầy");
+            else if (BMI >= 16 && BMI < 18.5)
+                Console.WriteLine("Thân hình hơi gầy");
+            else if (BMI >= 18.5 && BMI < 25)
+                Console.WriteLine("Thân hình bình thường");
+            else if (BMI >= 25 && BMI < 30)
+                Console.WriteLine("Thân hình hơi béo");
+            else if (BMI >= 30 && BMI < 35)
+                Console.WriteLine("Thân hình béo");
+            else
+                Console.WriteLine("Thân hình quá béo");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise_Thuong/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Thuong
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            int numberChooseEx = 0;
+            InDeBai();
+            ChooseExercise(numberChooseEx);
+        }
+
+        #region Method
+        static void InDeBai()
+        {
+            string bai1 = "Bai 1";
+            string bai2 = "Bai 2";
+            string bai3 = "Bai 3";
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(bai1);
+            Console.WriteLine(bai2);
+            Console.WriteLine(bai3);
+            Console.WriteLine("--------------------------");
+        }
+        static void ChooseExercise(int i) 
+        {
+            BT_If_else bt = new BT_If_else();
+            Console.Write("Nhập số để chọn kết quả bài tập: ");
+            i = int.Parse(Console.ReadLine());
+            while (i < 0 || i > 10)
+            {
+                NhapSoKhongHopLe(i);
+                ChooseExercise(i);
+            }
+            switch (i)
+            {
+                case 1:
+                    bt.ex_5();
+                    Console.ReadLine();
+                    break;
+            }
+        }
+
+        static void NhapSoKhongHopLe(int i) 
+        {
+            Console.Write("Số {0} bạn nhập không hợp lệ! Vui lòng nhập lại: ", i);
+        }
+
+        
+        #endregion
+    }
+}
