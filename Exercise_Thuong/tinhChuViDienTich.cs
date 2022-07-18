09ca837d1aa1c4b7a7ba63e65b037ed5a28d43ff
Nguyễn Thanh Long <ntl.284@gmail.com>
2022-07-18 19:27:07 +0700

    -220718 tinh PS

---
+++ Exercise_Thuong/tinhChuViDienTich.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Thuong
+{
+    public class tinhChuViDienTich
+    {
+        public void ex1()
+        {
+            float dai, rong, chuVi, dienTich;
+            Console.WriteLine("Nhap chieu dai:");
+            dai = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu rong");
+            rong = float.Parse(Console.ReadLine());
+            chuVi = (dai + rong) * 2;
+            dienTich = dai * rong;
+            Console.WriteLine("Chu vi la: " + chuVi);
+            Console.WriteLine("Dien tich la: " +dienTich);
+        }
+    }
+}
